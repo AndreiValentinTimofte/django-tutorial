a1a07d50d49b66f41c02d0b42013092bdb5294cc
Andrei Valentin Timofte <ukg15446@kireygroup.com>
2025-07-28 16:45:39 +0200

    feat(polls): Customize admin site for Question and Choice models

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'question', 'votes')
+
+
+admin.site.register(Choice,ChoiceAdmin)
