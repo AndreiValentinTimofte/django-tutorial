ef020890f1dc55e86c6c5587012e9fe94978e9cd
timo <valentin.timofte00@gmail.com>
2025-07-28 16:49:23 +0200

    create a conflict with home pc

--- polls/admin.py
+++ polls/admin.py
@@ -6,6 +6,8 @@ from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    # pc casa
+
     fields = ['pub_date', 'question_text']
 
     list_display = ('question_text', 'pub_date', 'was_published_recently')
