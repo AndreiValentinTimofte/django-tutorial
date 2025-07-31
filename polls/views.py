33f792ceba8795424d1b1f85bd9d7266f6e25a00
Andrei Valentin Timofte <ukg15446@kireygroup.com>
2025-07-31 08:49:27 +0200

    feat(polls): Implement dynamic index view and template

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 # polls/views.py
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
+
+
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Ciao, Valen! La tua app Django 'polls' funziona!")
+
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'lastest_question_list': lastest_question_list,
+    }
+
+    return render(request, 'polls/index.html', context)
+
+
+
+    # return HttpResponse("Ciao, Valen! La tua app Django 'polls' funziona!")
