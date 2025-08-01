33207ed79ec6c9c67e88b412be49d680dab60614
Andrei Valentin Timofte <ukg15446@kireygroup.com>
2025-08-01 08:51:01 +0200

    refactor(polls): Use generic views for index, detail and result

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = "polls" # Questo è per il namespacing degli URL, utile in progetti più grandi
 urlpatterns = [
-    path("", views.index, name="index"), # Questo mappa l'URL vuoto (es. /polls/) alla vista 'index'
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"), # Questo mappa l'URL vuoto (es. /polls/) alla vista 'index'
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>", views.ResultView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -9,26 +11,49 @@ from .models import Question, Choice
 # Create your views here.
 
 
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'lastest_question_list': lastest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
-    # return HttpResponse("Ciao, Valen! La tua app Django 'polls' funziona!")
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question': question,
-    }
-    return render(request, 'polls/detail.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def index(request):
+#     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'lastest_question_list': lastest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
+#     # return HttpResponse("Ciao, Valen! La tua app Django 'polls' funziona!")
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {
+#         'question': question,
+#     }
+#     return render(request, 'polls/detail.html', context)
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
